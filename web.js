c675d74687362baa099d3d4d35223341ff81d3ab
bhandzo <ben.handzo@gmail.com>
2013-07-19 18:42:12 -0700

    ripped up old index html and re-styled with skeleton. Changed to express.staic

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
 var fs = require('fs');
 var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/'));
+// var app = express.createServer(express.logger());
 
-var app = express.createServer(express.logger());
+// var index = fs.readFileSync('index.html')
 
-var index = fs.readFileSync('index.html')
-
-app.get('/', function(request, response) {
-  response.send(index.toString('utf-8'));
-});
+// app.get('/', function(request, response) {
+//   response.send(index.toString('utf-8'));
+// });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
